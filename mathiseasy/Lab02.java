71768e9c581616302a0f69639a33d8a934b0ea13
Alex <brokenmouseinfo@gmail.com>
2018-03-04 21:06:53 -0800

    Initial commit

---
+++ mathiseasy/Lab02.java
@@ -0,0 +1,35 @@
+import comp102x.IO;
+
+public class Lab02
+{
+    
+    public static void multiply()
+    {
+        // Please write your code after this line
+        
+        
+        
+        
+        
+    }
+    
+    public static void calculateTriangleArea()
+    {
+        // Please write your code after this line
+        
+        
+        
+        
+        
+    }
+    
+    public static void solveQuadraticEquation()
+    {
+        // Please write your code after this line
+        
+        
+        
+        
+        
+    }
+}
