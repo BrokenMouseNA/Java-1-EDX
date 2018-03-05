e045c6bc32f5befb1035280d2cba0641b3a8b9b4
Alex <brokenmouseinfo@gmail.com>
2018-03-04 22:30:31 -0800

    math
    simple lab  that did triangle, quad formula, and muliply

--- mathiseasy/Lab02.java
+++ mathiseasy/Lab02.java
@@ -2,12 +2,21 @@ import comp102x.IO;
 
 public class Lab02
 {
-    
+
     public static void multiply()
     {
         // Please write your code after this line
+        float xInput;
+        float yInput;
+        float answer;
+        IO.output("Please enter x: ");
+        xInput = IO.inputFloat();
+        IO.output("Please enter y: ");
+        yInput = IO.inputFloat();
+        answer = xInput * yInput;
         
-        
+        IO.outputln("Your answer is " + answer);
+             
         
         
         
@@ -16,6 +25,18 @@ public class Lab02
     public static void calculateTriangleArea()
     {
         // Please write your code after this line
+        float widthInput;
+        float heightInput;
+        float answer;
+        
+        IO.output("Enter the width of the triangle: ");
+        widthInput = IO.inputFloat();
+        IO.output("Enter the height of the triangle: ");
+        heightInput = IO.inputFloat();
+        answer = (widthInput * heightInput) / 2;
+        
+        IO.outputln("The area of the triangle is: " + answer);
+        
         
         
         
@@ -26,7 +47,36 @@ public class Lab02
     public static void solveQuadraticEquation()
     {
         // Please write your code after this line
+        //No scanner was used. Just a simple solution.
+        float a, b, c;
+       
+        double firstSolution,secondSolution, x, discrim;
+        IO.output("Enter a:");
         
+        a = IO.inputFloat();
+        
+        IO.output("Enter b:");
+        
+        b = IO.inputFloat();
+        
+        IO.output("Enter c:");
+        
+        c = IO.inputFloat();
+        
+        //Find the discriminate
+        
+        discrim = b*b -4*a*c;
+        
+        //Calculate both sets
+        
+        firstSolution = ( - b + Math.sqrt(discrim))/(2*a);
+        
+        secondSolution = (- b - Math.sqrt(discrim))/(2*a);
+        
+        //Output
+        IO.outputln("Fist solution for x: " +firstSolution);
+        
+        IO.outputln("Second solution for x: " +secondSolution);
         
         
         
